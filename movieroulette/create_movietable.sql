2a575e44b51fd770df6f3b4b7071893c5da68daa
Jacob Siegumfeldt <jacobi7th@gmail.com>
2024-05-30 13:46:00 +0200

    Små ændringer.

--- movieroulette/create_movietable.sql
+++ movieroulette/create_movietable.sql
@@ -2,12 +2,12 @@ DROP TABLE IF EXISTS Movies;
 
 CREATE TABLE Movies(id varchar(10),
 year int,
-primaryTitle char(200),
-originalTitle char(200),
+primaryTitle varchar(200),
+originalTitle varchar(200),
 runtimeMinutes int, 
-genres char(60),
+genres varchar(60),
 averageRating float,
-CONSTRAINT mv PRIMARY KEY (id));
+PRIMARY KEY (id));
 
 copy Movies(id,year,primaryTitle,originalTitle,runtimeMinutes,genres,averageRating)
             from '/Users/jacobsiegumfeldt/Desktop/DIS/Project/movieroulette/tmp/imdb_movie_list_scraped.csv'
