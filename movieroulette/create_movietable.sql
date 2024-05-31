f31a73d86d58dd5606198d94474c5df674c7aeda
Jacob Siegumfeldt <jacobi7th@gmail.com>
2024-05-31 12:56:35 +0200

    Tilføjet posterURL som attribute i Movies.

--- movieroulette/create_movietable.sql
+++ movieroulette/create_movietable.sql
@@ -1,15 +1,18 @@
 DROP TABLE IF EXISTS Movies;
 
-CREATE TABLE Movies(id varchar(10),
-year int,
-primaryTitle varchar(200),
-originalTitle varchar(200),
-runtimeMinutes int, 
-genres varchar(60),
-averageRating float,
-PRIMARY KEY (id));
+CREATE TABLE Movies(
+    id varchar(10),
+    year int,
+    primaryTitle varchar(255),
+    originalTitle varchar(255),
+    runtimeMinutes int, 
+    genres varchar(60),
+    averageRating float,
+    posterURL varchar(255),
+    PRIMARY KEY (id)
+);
 
-copy Movies(id,year,primaryTitle,originalTitle,runtimeMinutes,genres,averageRating)
-            from '/Users/jacobsiegumfeldt/Desktop/DIS/Project/movieroulette/tmp/imdb_movie_list_scraped.csv'
-            delimiter ','
-            CSV HEADER;
+COPY Movies(id,year,primaryTitle,originalTitle,runtimeMinutes,genres,averageRating,posterURL)
+    FROM '/Users/jacobsiegumfeldt/Desktop/DIS/Project/movieroulette/tmp/imdb_movie_list.csv'
+    delimiter ','
+    CSV HEADER;
